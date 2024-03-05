c582c80f6984db5f57811483edf2c5f5f06774a8
Ulan <ulanr92@gmail.com>
2024-03-04 22:43:42 -0600

    last commit

--- 06_generics/src/index.ts
+++ 06_generics/src/index.ts
@@ -1,7 +1,4 @@
-// Generics
-
-
-
+/* ---- Generics ---- */
 interface User {
     id: number,
     email: string
@@ -22,7 +19,7 @@ const user = clone({id: 1, email: 'user@mail.com'})
 const dog = clone({name: 'Rocky', age: 2}); // Object literal may only specify known properties, and 'name' does not exist in type 'User'.
 
 
-// Solution might generics
+/* ---- Solution might be generics ---- */
 
 function clone2<T>(obj: T): T { 
     return Object.apply({}, obj);
@@ -33,12 +30,13 @@ const user1 = clone2({id: 1, email: 'user@mail.com'})
 const cat1 = clone2({name: 'Roxana', age: 1, city: 'Chicago'}); 
 
 
-// Multiple generics
+/* ---- Multiple generics ---- */
 function clone3<T, U>(obj: T, obj1: U): T | U { 
     return Object.apply({}, obj);
 }
 
-// Multiple generics
-function getData() { 
+// Not related example
+// Return type Promise
+function getData(): Promise<{data: string}> { 
         return Promise.resolve({data: 'data'}); 
 }
